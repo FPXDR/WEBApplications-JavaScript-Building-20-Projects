afd1b043107242a7a82471a92fdbe49281fdf9da
FPXD <ranvlp@hotmail.com>
2024-04-13 02:28:01 +0700

    Finished

---
+++ Clicktocopy/app.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector(".btn")
+const coupon = document.querySelector(".coupon")
+
+
+btn.addEventListener("click",(e)=>{
+    coupon.select()
+    coupon.setSelectionRange(0,99999)
+    navigator.clipboard.writeText(coupon.value)
+    btn.textContent="คัดลอกคูปองแลัว"
+    setTimeout(()=>{
+        btn.textContent="คัดลอก"
+    },2500)
+})
