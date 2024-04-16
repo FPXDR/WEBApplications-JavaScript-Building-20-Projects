3a9f0bde783cd087003fab3710009ab3b27ae5ab
FPXD <ranvlp@hotmail.com>
2024-04-17 02:40:37 +0700

    7

---
+++ VowelsCounting/app.js
@@ -0,0 +1,14 @@
+const countEI = document.querySelector(".count");
+const input = document.querySelector("input");
+
+input.addEventListener("keyup",()=>{
+    let word = input.value.toLowerCase()
+    let vowelCount = 0
+    for(let i = 0;i<word.length;i++){
+        let letter = word[i]
+        if(letter.match(/([a,e,i,o,u])/)){
+            vowelCount++
+        }
+    }
+    countEI.innerHTML= `${vowelCount}`
+})
