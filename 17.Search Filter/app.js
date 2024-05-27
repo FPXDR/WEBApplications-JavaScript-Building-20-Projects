c59df06addb9f3ac14b4cc9d352a8d474b09999d
FPXD <ranvlp@hotmail.com>
2024-05-27 19:14:03 +0700

    update

--- 17.Search Filter/app.js
+++ 17.Search Filter/app.js
@@ -1,7 +1,17 @@
+/* The code snippet is declaring three variables using `const` keyword:
+1. `result`: It is storing the reference to the HTML element with the id "result". This element is
+typically used to display the search results or data fetched from an API.
+2. `filter`: It is storing the reference to the HTML element with the id "filter". This element is
+usually an input field where users can type to filter the displayed data.
+3. `listItem`: It is an empty array that will be used to store references to dynamically created
+list items (li elements) that display country information fetched from an API. */
 const result = document.getElementById("result")
 const filter = document.getElementById("filter")
 const listItem = []
 
+/* The code snippet you provided is adding an event listener to the `filter` input field. When a user
+types into the filter input field, this event listener triggers a function that performs the
+following actions: */
 filter.addEventListener("input",(e)=>{
     // thailand => thailand
     const search = e.target.value.toLowerCase()
