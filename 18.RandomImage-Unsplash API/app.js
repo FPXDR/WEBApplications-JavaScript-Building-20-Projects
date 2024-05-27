a9b9d0d488ad1903e0f6ec0425888fbe75b66442
FPXD <ranvlp@hotmail.com>
2024-05-27 20:54:34 +0700

    end

--- 18.RandomImage-Unsplash API/app.js
+++ 18.RandomImage-Unsplash API/app.js
@@ -1,13 +1,13 @@
 const conteainer = document.querySelector(".conteainer")
-const row = 4
-const cols = 5
+const row = 6
+const cols = 4
 
 function randomNumber(){
     return Math.floor(Math.random()*500)
 }
 for(let i = 0 ;i<row*cols;i++){
     //link
-    const url = `https://source.unsplash.com/random/${randomNumber()}`
+    const url = `https://picsum.photos/seed/picsum/1366/720`
     //พื้นที่แสดงภาพ
     const imageEI = document.createElement("img")
     imageEI.src=url
